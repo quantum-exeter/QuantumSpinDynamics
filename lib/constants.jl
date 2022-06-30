576677ceb7724b4f82bd81f3fae39c04b3e0eb9e
Charlie <charlotte.hogg@me.com>
2022-06-30 10:10:08 +0100

    Constants file merged with variables.

--- lib/constants.jl
+++
@@ -1,8 +0,0 @@
-######################
-#### constants.jl ####
-######################
-
-γ = -1.76*10^(11) # Gyromagnetic ratio for an electron (T^-1s^-1)
-Bext = 10 # External magnetic field (T)
-ωL = abs(γ)*Bext # Larmor frequency (s^-1)
-Λ = 10^10 # Cutoff frequency for spectral density
