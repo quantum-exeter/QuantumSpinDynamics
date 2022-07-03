90b6af4eff72285b1d11e99da23f6fe210cd14a2
Charlie <charlotte.hogg@me.com>
2022-07-04 00:07:02 +0100

    Gyromagnetic ratio set to 1.

--- lib/variables.jl
+++ lib/variables.jl
@@ -2,7 +2,7 @@
 #### variables.jl ####
 ######################
 
-γ = -1.76*10^(11) # Gyromagnetic ratio for an electron (T^-1s^-1)
+γ = -1 # Gyromagnetic ratio for an electron (T^-1s^-1)
 Bext = 10 # External magnetic field (T)
 ωL = abs(γ)*Bext # Larmor frequency (s^-1)
 Λ = 10^10 # Cutoff frequency for spectral density
@@ -29,15 +29,15 @@ prm1 = prme; # Change the RHS here to change parameter set for coupling directio
 prm2 = prme; # Change the RHS here to change parameter set for coupling direction 2
 prm3 = prme; # Change the RHS here to change parameter set for coupling direction 3
 
-ω01, Γ1, α1 = prm1*ωL
-ω02, Γ2, α2 = prm2*ωL
-ω03, Γ3, α3 = prm3*ωL
+ω01, Γ1, α1 = prm1
+ω02, Γ2, α2 = prm2
+ω03, Γ3, α3 = prm3
 
 ### RC-Specific Parameters ###
 
 Ω1, Ω2, Ω3 = [ω01 ω02 ω03]
-λ1, λ2, λ3 = ωL*[sqrt(α1/Ω1) sqrt(α2/Ω2) sqrt(α3/Ω3)]
-δ1, δ2, δ3 = [Γ1/ωL Γ2/ωL Γ3/ωL]
+λ1, λ2, λ3 = [sqrt(α1/Ω1) sqrt(α2/Ω2) sqrt(α3/Ω3)]
+δ1, δ2, δ3 = [Γ1 Γ2 Γ3]
 n1, n2, n3 = [3 3 1] # Change the number of RC levels here
 
 δ_list(dim) = [δ1 δ2 δ3][dim] # List of dissipation strengths for Dynamics.jl module
