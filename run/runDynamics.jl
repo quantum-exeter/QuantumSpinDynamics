3e90ea7692020da608f34267019d74f2063c782e
Charlie Hogg <charlotte.hogg@me.com>
2022-07-06 22:23:55 +0100

    Differential equation solver moved into separate file.

--- run/runDynamics.jl
+++ run/runDynamics.jl
@@ -4,21 +4,19 @@ using DataFrames
 include("../lib/Dynamics.jl")
 using .Dynamics
 
-prm = LorPrm1D(2.0, 0.001, 10.0)
-ang =  CouplAng1D(0.0, 0.0)
-n = Lev1D(2)
-T = 10
+### Parameters ###
+prm = LorPrm1D(2.0, 0.001, 10.0) # Lorentzian parameters
+ang =  CouplAng1D(0.0, 0.0) # Coupling angles
+n = Lev1D(2) # Number of RC levels
+T = 10 # Temperature
 
 ### Time Range ###
 ti, tf, dt = [0 100 2];
 tspan = (ti, tf);
 t = ti:dt:tf;
 
-using LinearAlgebra
-### z-Magnetisations ###
-ρ = dsolve(prm, ang, n, T, tspan)
-tr(ρ(0))
+sz_list = [szDyn(prm, ang, n, T, tspan, i) for i in tspan]
 
 ### Store Values ###
-df = DataFrame(hcat(T, magz_list), :auto)
+df = DataFrame(hcat(T, sz_list), :auto)
 CSV.write("C://Users//crh222//Data//Classical//test.csv",  df, header = ["T", "sz"]);
