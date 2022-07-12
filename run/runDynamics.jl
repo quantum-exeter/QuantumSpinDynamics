bd2eef040a3ec746392f647b0589e3d3f8c0c371
Charlie Hogg <charlotte.hogg@me.com>
2022-07-12 11:50:39 +0100

    Changes made in line with dynamics speed up.

--- run/runDynamics.jl
+++ run/runDynamics.jl
@@ -5,18 +5,21 @@ include("../lib/Dynamics.jl")
 using .Dynamics
 
 ### Parameters ###
-prm = LorPrm1D(2.0, 0.001, 10.0) # Lorentzian parameters
-ang =  CouplAng1D(0.0, 0.0) # Coupling angles
-n = Lev1D(2) # Number of RC levels
-T = 10 # Temperature
+prm = LorPrm2D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0) # Lorentzian parameters
+ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
+n = Lev2D(3, 3) # Number of RC levels
+T = 0.01 # Temperature
 
 ### Time Range ###
-ti, tf, dt = [0 100 2];
+ti, tf, dt = [7000 10000 100];
 tspan = (ti, tf);
 t = ti:dt:tf;
 
-sz_list = [szDyn(prm, ang, n, T, tspan, i) for i in tspan]
+szDyn(ρ(1), n)
+
+ρ = dsolve(prm, ang, n, T, tspan)
+sz_list = [realIfClose(szDyn(ρ(i), n)) for i in t]
 
 ### Store Values ###
-df = DataFrame(hcat(T, sz_list), :auto)
-CSV.write("C://Users//crh222//Data//Classical//test.csv",  df, header = ["T", "sz"]);
+df = DataFrame(hcat(t, sz_list), :auto)
+CSV.write("C://Users//crh222//Dropbox//PhD//1. RC Mapping//Dynamics Data//test.csv",  df, header = ["T", "sz"]);
