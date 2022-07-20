17a943e97f323956b8bc787f18f665b6468c4a58
Charlie Hogg <charlotte.hogg@me.com>
2022-07-21 00:30:24 +0100

    Uneccessary function calls removed.

--- run/runDynamics.jl
+++ run/runDynamics.jl
@@ -6,20 +6,18 @@ using .Dynamics
 
 ### Parameters ###
 prm = LorPrm3D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0, 2.0, 0.001, 10.0) # Lorentzian parameters
-ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
-n = Lev2D(3, 3) # Number of RC levels
-T = 0.01 # Temperature
+ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
+n = Lev3D(3, 3, 3) # Number of RC levels
+T = 10 # Temperature
 
 ### Time Range ###
-ti, tf, dt = [7000 10000 100];
+ti, tf, dt = [8000 11000 100];
 tspan = (ti, tf);
 t = ti:dt:tf;
-
-szDyn(ρ(1), n)
 
 ρ = dsolve(prm, ang, n, T, tspan)
 sz_list = [realIfClose(szDyn(ρ(i), n)) for i in t]
 
 ### Store Values ###
 df = DataFrame(hcat(t, sz_list), :auto)
-CSV.write("C://Users//crh222//Dropbox//PhD//1. RC Mapping//Dynamics Data//test.csv",  df, header = ["T", "sz"]);
+CSV.write("C://Users//crh222//Dropbox//PhD//1. RC Mapping//Dynamics Data//3d_prma_10_3.csv",  df, header = ["T", "sz"]);
