7ac7d8acdedbb41e4aa9b9105858ec6952f778d9
Charlie Hogg <charlotte.hogg@me.com>
2022-07-21 00:28:20 +0100

    Comparison files added with fidelity calculations.

---
+++ lib/Comparison.jl
@@ -0,0 +1,32 @@
+module Comparison
+
+    ### Import Packages ###
+    using LinearAlgebra
+    using Kronecker
+    using DifferentialEquations
+    using SparseArrays
+
+    ### Inclusions ###
+    include("variables.jl")
+    include("diffEqSolver.jl")
+    include("hamiltonians.jl")
+    include("magnetisations.jl")
+    include("maths.jl")
+    include("operators.jl")
+    include("spectralDensity.jl")
+    include("states.jl")
+    include("superoperators.jl")
+    include("transitions.jl")
+
+    ####################################
+    ####################################
+    ####################################
+
+    ### Exports ###
+    
+    export LorPrm1D, LorPrm2D, LorPrm3D,
+           CouplAng1D, CouplAng2D, CouplAng3D,
+           Lev1D, Lev2D, Lev3D,
+           ρMFGS, dsolve, ptrace, realIfClose
+
+end
