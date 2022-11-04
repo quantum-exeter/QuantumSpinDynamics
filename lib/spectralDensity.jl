1811e924b31725b0834af8c980d5b3fcab39d9e2
Hogg <charlotte.hogg@me.com>
2022-11-04 13:25:37 +0000

    Lorentzian spectral density formulae corrected.

--- lib/spectralDensity.jl
+++ lib/spectralDensity.jl
@@ -11,5 +11,5 @@ spectral_density_Ohm(ω, prm::LorPrm3D) = [((prm.Γ1*ω)/π)*(Λ^2/(ω^2 + Λ^2)
                                       ((prm.Γ2*ω)/π)*(Λ^2/(ω^2 + Λ^2))
                                       ((prm.Γ3*ω)/π)*(Λ^2/(ω^2 + Λ^2))]
 
-spectral_density_Lor(ω, prm::LorPrm1D) = ((prm.α1*prm.Γ1*ω)/π)*(ω/((prm.ω01^2 - ω^2)^2 + prm.Γ1^2*ω^2))
-spectral_density_Lor_inv(ω, prm::LorPrm1D) = ((prm.α1*prm.Γ1)/π)*(ω/((prm.ω01^2 - ω^2)^2 + prm.Γ1^2*ω^2))
+spectral_density_Lor(ω, prm::LorPrm1D) = ((prm.α1*prm.Γ1*ω)/π)*(1/((prm.ω01^2 - ω^2)^2 + prm.Γ1^2*ω^2))
+spectral_density_Lor_inv(ω, prm::LorPrm1D) = ((prm.α1*prm.Γ1)/π)*(1/((prm.ω01^2 - ω^2)^2 + prm.Γ1^2*ω^2))
