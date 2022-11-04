2f73c5736fb551119eb33b1c6b4b4fbb42aa6992
Hogg <charlotte.hogg@me.com>
2022-11-04 13:24:05 +0000

    Reverted back to non-log method to calculate expectation values.

--- lib/SDintegrals.jl
+++ lib/SDintegrals.jl
@@ -6,10 +6,10 @@ function 𝒬(prm::LorPrm1D)
   return quadgk(ω -> spectral_density_Lor_inv(ω, prm), 0.0, Inf)[1]
 end
 
-function Σ(prm::LorPrm1D, ωL)
-  I(ω) = spectral_density_Lor(ω, prm)*ω/(ω + ωL)
+function Σ(prm::LorPrm1D)
+  I(ω) = spectral_density_Lor(ω, prm)*ω/(ω +1)
   Il = quadgk_cauchy(I, 0.0, 1.0, 2.0)[1]
-  Ir = quadgk(ω -> I(ω)/(ω - ωL), 2.0, Inf)[1]
+  Ir = quadgk(ω -> I(ω)/(ω - 1), 2.0, Inf)[1]
   return Il + Ir
 end
   
@@ -20,10 +20,10 @@ function Σ′(prm::LorPrm1D)
   return Il + Ir
 end
 
-function Δ(prm::LorPrm1D, β, ωL)
-  I(ω) = ωL*spectral_density_Lor_inv(ω, prm)*xcoth(β*ω/2)/(β/2)/(ω + ωL)
+function Δ(prm::LorPrm1D, β)
+  I(ω) = spectral_density_Lor_inv(ω, prm)*xcoth(β*ω)/(β)/(ω + 1)
   Il = quadgk_cauchy(I, 0.0, 1.0, 2.0)[1]
-  Ir = quadgk(ω -> I(ω)/(ω - ωL), 2.0, Inf)[1]
+  Ir = quadgk(ω -> I(ω)/(ω - 1), 2.0, Inf)[1]
   return Il + Ir
 end
 
@@ -33,3 +33,4 @@ function Δ′(prm::LorPrm1D, β)
   Ir = quadgk(ω -> I(ω)/(ω - 1)^2, 2.0, Inf)[1]
   return Il + Ir
 end
+
