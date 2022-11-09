a683721da6230cfdbfcdba11d450a590b3857a49
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-09 14:52:22 +0000

    Mac export added.

--- run/runStatics.jl
+++ run/runStatics.jl
@@ -6,12 +6,12 @@ using .Statics
 
 #prma = 2.0, 0.001, 10.0
 #prmb = 2.0, 0.001, 1.0
-#prmUS = 2.0, 0.001, 1000
+#prmc = 2.0, 0.001, 0.1
 
 ### Parameters ###
 # prm = LorPrm1D(2.0, 0.001, 10.0) 
 # prm = LorPrm2D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0) 
-prm = LorPrm3D(2.0, 0.001, 1.0, 2.0, 0.001, 1.0, 2.0, 0.001, 1.0) # Lorentzian parameters
+prm = LorPrm3D(2.0, 0.001, 0.1, 2.0, 0.001, 0.1, 2.0, 0.001, 0.1) # Lorentzian parameters
 # ang =  CouplAng1D(0.0, 0.0) # Coupling angles
 # ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
 ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
@@ -33,7 +33,7 @@ szMFGS_list = [realIfClose(szMFGS(prm, ang, n, i)) for i in T]
 ### Store Values ###
 
 dfGibbs = DataFrame(hcat(T, sxG_list, syG_list, szG_list), :auto)
-CSV.write("C://Users//crh222//Data//RC//Quantum//Statics//Gibbs.csv",  dfGibbs, header = ["T", "sxG", "syG", "szG"])
+CSV.write("Gibbs.csv",  dfGibbs, header = ["T", "sxG", "syG", "szG"])
 
 dfMFGS = DataFrame(hcat(T, sxMFGS_list, syMFGS_list, szMFGS_list), :auto)
-CSV.write("C://Users//crh222//Data//RC//Quantum//Statics//MFGS_3D_prmb_5.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
+CSV.write("/Users/charliehogg/Work/QuantumSpinDynamics/paper_data/MFGS_WK_prmc.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
