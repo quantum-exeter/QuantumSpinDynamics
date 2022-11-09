8ea8b64e810dae67c537605473e57b329c250883
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-09 14:53:29 +0000

    Parameter sets altered and Mac export added.

--- run/runWK.jl
+++ run/runWK.jl
@@ -6,10 +6,10 @@ using .Statics
 
 #prma = 2.0, 0.001, 10.0
 #prmb = 2.0, 0.001, 1.0
-#prmUS = 2.0, 0.001, 1000
+#prmc = 2.0, 0.001, 0.1
 
 ### Parameters ###
-prm = LorPrm1D(2., 0.001, 1.) 
+prm = LorPrm1D(2., 0.001, 0.1) 
 
 ### Temperature Range ###
 T = exp10.(range(-3, 3, length=20))
@@ -18,4 +18,4 @@ szWK_list = [realIfClose(szWK(1/i, prm)) for i in T]
 
 ### Store Values ###
 dfWK = DataFrame(hcat(T, szWK_list), :auto)
-CSV.write("C://Users//crh222//Data//3DProj//Quantum//Statics//WK.csv",  dfWK, header = ["T", "szWK"])
+CSV.write("/Users/charliehogg/Work/QuantumSpinDynamics/paper_data/WK_prmc.csv",  dfWK, header = ["T", "szWK"])
