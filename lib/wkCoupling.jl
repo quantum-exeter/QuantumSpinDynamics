10c7d945f082c97c6f932397282d2965828a14d2
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-09 14:50:36 +0000

    Partition function and derivative corrected. Zero-temp function added.

--- lib/wkCoupling.jl
+++ lib/wkCoupling.jl
@@ -10,7 +10,13 @@ I1′(prm::LorPrm1D) = Σ′(prm)
 I2′(prm::LorPrm1D) = -Σ′(prm)
 I3′(prm::LorPrm1D, β) = Δ′(prm, β)
 
-𝒵(prm::LorPrm1D, β) = 2*big(cosh(β))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) - 2*big(sinh(β))*β*I3(prm, β)
-𝒵′(prm::LorPrm1D, β) = 2*β*big(sinh(β))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) + 2*big(cosh(β))*(3*β*I1′(prm) + β*I2′(prm)) - 2*β^2*big(cosh(β))*I3(prm, β) - 2*β*big(sinh(β))*I3′(prm, β)
+function I4′(prm::LorPrm1D)
+    I(ω) = spectral_density_Lor(ω, prm)/(ω + 1)^2
+    return -quadgk(ω -> I(ω), 0.0, Inf)[1]
+end
+
+𝒵(prm::LorPrm1D, β) = 2*big(cosh(β/2))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) - big(sinh(β/2))*β*I3(prm, β)
+𝒵′(prm::LorPrm1D, β) = β*big(sinh(β/2))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) + 2*big(cosh(β/2))*(0.75*β*I1′(prm) + 0.25*β*I2′(prm)) - 0.5*β^2*big(cosh(β/2))*I3(prm, β) - β*big(sinh(β/2))*I3′(prm, β)
 
 szWK(β, prm::LorPrm1D) = (1/β)*(1/𝒵(prm, β))*𝒵′(prm, β)
+szWKZT(prm::LorPrm1D) = 1 + 2*I4′(prm)
