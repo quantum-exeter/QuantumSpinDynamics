8ee944fec137aea89dc16306667b600286329aac
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-11 16:10:56 +0000

    big() added around functions as an attempt to treat low-T numerical issues.

--- lib/wkCoupling.jl
+++ lib/wkCoupling.jl
@@ -15,8 +15,8 @@ function I4′(prm::LorPrm1D)
     return -quadgk(ω -> I(ω), 0.0, Inf)[1]
 end
 
-𝒵(prm::LorPrm1D, β) = 2*big(cosh(β/2))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) - big(sinh(β/2))*β*I3(prm, β)
-𝒵′(prm::LorPrm1D, β) = β*big(sinh(β/2))*(1 + 0.75*β*I1(prm) + 0.25*β*I2(prm)) + 2*big(cosh(β/2))*(0.75*β*I1′(prm) + 0.25*β*I2′(prm)) - 0.5*β^2*big(cosh(β/2))*I3(prm, β) - β*big(sinh(β/2))*I3′(prm, β)
+𝒵(prm::LorPrm1D, β) = 2*(cosh(big(β)/2))*(1 + 0.75*big(β)*I1(prm) + 0.25*big(β)*I2(prm)) - (sinh(big(β)/2))*big(β)*I3(prm, β)
+𝒵′(prm::LorPrm1D, β) = big(β)*(sinh(big(β)/2))*(1 + 0.75*big(β)*I1(prm) + 0.25*big(β)*I2(prm)) + 2*(cosh(big(β)/2))*(0.75*big(β)*I1′(prm) + 0.25*big(β)*I2′(prm)) - 0.5*big(β)^2*(cosh(big(β)/2))*I3(prm, β) - big(β)*(sinh(big(β)/2))*I3′(prm, β)
 
-szWK(β, prm::LorPrm1D) = (1/β)*(1/𝒵(prm, β))*𝒵′(prm, β)
+szWK(β, prm::LorPrm1D) = (1/big(β))*(1/𝒵(prm, β))*𝒵′(prm, β)
 szWKZT(prm::LorPrm1D) = 1 + 2*I4′(prm)
