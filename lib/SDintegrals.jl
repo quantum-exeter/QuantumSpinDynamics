220ea6d13fd72f852f073bbe6ea4521276dc6983
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-11 16:05:40 +0000

    Argument of coth() corrected.

--- lib/SDintegrals.jl
+++ lib/SDintegrals.jl
@@ -21,14 +21,14 @@ function Σ′(prm::LorPrm1D)
 end
 
 function Δ(prm::LorPrm1D, β)
-  I(ω) = spectral_density_Lor_inv(ω, prm)*xcoth(β*ω)/(β)/(ω + 1)
+  I(ω) = spectral_density_Lor_inv(ω, prm)*xcoth(β*ω/2)/(β/2)/(ω + 1)
   Il = quadgk_cauchy(I, 0.0, 1.0, 2.0)[1]
   Ir = quadgk(ω -> I(ω)/(ω - 1), 2.0, Inf)[1]
   return Il + Ir
 end
 
 function Δ′(prm::LorPrm1D, β)
-  I(ω) = spectral_density_Lor_inv(ω, prm)*(ω^2 + 1)*xcoth(β*ω)/(β)/(ω + 1)^2
+  I(ω) = spectral_density_Lor_inv(ω, prm)*(ω^2 + 1)*xcoth(β*ω/2)/(β/2)/(ω + 1)^2
   Il = quadgk_hadamard(I, 0.0, 1.0, 2.0)[1]
   Ir = quadgk(ω -> I(ω)/(ω - 1)^2, 2.0, Inf)[1]
   return Il + Ir
