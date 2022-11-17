c37b803d7f6b010e3abe1f3a4ed851bbd3c1002d
Charlie Hogg <charliehogg@Charlies-MacBook-Pro.local>
2022-11-17 20:09:26 +0000

    Increased gamma parameter set applied to weak coupling limit.

--- run/runWK.jl
+++ run/runWK.jl
@@ -4,12 +4,11 @@ using DataFrames
 include("../lib/Statics.jl")
 using .Statics
 
-#prma = 2.0, 0.001, 10.0
-#prmb = 2.0, 0.001, 1.0
-#prmc = 2.0, 0.001, 0.1
+#prmc = 2., 0.001, 0.1
+#prmf = 2., 0.6, 0.1
 
 ### Parameters ###
-prm = LorPrm1D(2., 0.001, 0.1) 
+prm = LorPrm1D(2., 0.6, 0.1) 
 
 ### Temperature Range ###
 T = exp10.(range(-3, 3, length=20))
@@ -18,4 +17,4 @@ szWK_list = [realIfClose(szWK(1/i, prm)) for i in T]
 
 ### Store Values ###
 dfWK = DataFrame(hcat(T, szWK_list), :auto)
-CSV.write("/Users/charliehogg/Work/QuantumSpinDynamics/paper_data/WK_prmc.csv",  dfWK, header = ["T", "szWK"])
+CSV.write("/Users/charliehogg/Work/QuantumSpinDynamics/paper_data/WK_prmf.csv",  dfWK, header = ["T", "szWK"])
