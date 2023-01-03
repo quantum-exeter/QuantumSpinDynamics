88e8f585a2bc7d4cf85b37e452ebb11247895b7a
Charlie Hogg <charliehogg@unknown.home>
2023-01-03 09:50:11 +0000

    Separate exports added for Mac/Windows.

--- run/runStatics.jl
+++ run/runStatics.jl
@@ -25,7 +25,7 @@ using .Statics
 ### Parameters ###
 # prm = LorPrm1D(2., 0.6, 1.) # Lorentzian parameters
 # prm = LorPrm2D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0) # Lorentzian parameters
-prm = LorPrm3D(2., 0.6, 500., 2., 0.6, 500., 2., 0.6, 500.) # Lorentzian parameters
+prm = LorPrm3D(2., 0.6, 40., 2., 0.6, 40., 2., 0.6, 40.) # Lorentzian parameters
 # ang =  CouplAng1D(π/4, 0.0) # Coupling angles
 # ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
 ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
@@ -45,10 +45,13 @@ szMFGS_list = [realIfClose(szMFGS(prm, ang, n, i)) for i in T]
 
 
 ### Store Values ###
-
 dfGibbs = DataFrame(hcat(T, sxG_list, syG_list, szG_list), :auto)
 CSV.write("Gibbs.csv",  dfGibbs, header = ["T", "sxG", "syG", "szG"])
 
 dfMFGS = DataFrame(hcat(T, sxMFGS_list, syMFGS_list, szMFGS_list), :auto)
-CSV.write("/Users/charliehogg/Dropbox/PhD/1. 3D Project/Data/Ultrastrong/qu_MFGS_3D_prmj_5.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"]) # Export for Mac
-# CSV.write("C:/Users/crh222/QuantumSpinDynamics/qu_MFGS_3D_prmf_5.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"]) # Export for Windows
+
+### Export for Mac ###
+CSV.write("/Users/charliehogg/Dropbox/PhD/1. 3D Project/Data/Ultrastrong/qu_MFGS_3D_prm40_5.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
+
+### Export for Windows ###
+CSV.write("C:/Users/crh222/QuantumSpinDynamics/qu_MFGS_3D_prm40_5.csv",  dfMFGS, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
