293eab23b163692aa65d6a01e4a8faf7612b793e
Charlie Hogg <charlotte.hogg@me.com>
2023-03-01 13:23:56 +0000

    Changes to weak coupling functions implemented.

--- run/runWK.jl
+++ run/runWK.jl
@@ -11,7 +11,6 @@ using .Statics
 #prma = 2., 0.001, 10.
 #prmb = 2., 0.001, 1. 
 #prmc = 2., 0.001, 0.1 
-
 #prmd = 2., 0.6, 10.
 #prme = 2., 0.6, 1.
 #prmf = 2., 0.6, 0.1
@@ -22,18 +21,21 @@ using .Statics
 #prmj = 2., 0.6, 500.
 #prmk = 2., 0.6, 1000.
 
-prm = LorPrm1D(2., 0.6, 0.1)
+# MAKE SURE TO SET ALPHA AS DOUBLE THE RC VALUE #
+prm = LorPrm1D(2., 0.6, 0.2)
 
 ## Temperature Range ##
-T = exp10.(range(-2, 3, length=100))
+T = exp10.(range(-2, 2, length=20))
 
-szWK_list = [realIfClose(szWK(1/i, prm)) for i in T]
+szWKZT(prm)
+
+szWK_list = [realIfClose(szWK(prm, 1/i)) for i in T]
 
 ### Store Values ###
 dfWK = DataFrame(hcat(T, szWK_list), :auto)
 
 ### Export for Mac ###
-# CSV.write("/Users/charliehogg/filename.csv",  dfMFGS, header = ["T", "szWK"])
+CSV.write("paper_data/WK_analytical_prmc.csv",  dfWK, header = ["T", "szWK"])
 
 ### Export for Windows ###
 # CSV.write("C:/Users/crh222/filename.csv",  dfMFGS, header = ["T", "szWK"])
