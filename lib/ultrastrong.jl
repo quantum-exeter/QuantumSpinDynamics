27e2be1a9ea7609210e15b4a69def01521b7a713
Charlie Hogg <charlotte.hogg@me.com>
2023-06-07 22:15:20 +0100

    osc_positions function cleaned up.

--- lib/ultrastrong.jl
+++ lib/ultrastrong.jl
@@ -2,21 +2,23 @@
 #### ultrastrong.jl ####
 ########################
 
+#### T=0 sz magnetisation for UNTRANSFORMED Hamiltonian ####
 function szTzero(prm::Lorentzian, ang::CouplingAngles, n::Levels)
-    H = HTot(prm, ang, n)
+    H = HTot(prm, ang, n) 
     F = eigen(H)
     ϵ, P = F.values, F.vectors
     E_ground = ϵ[1]
     sz = 0
-    σz′ = kronecker(σz, 𝕀(n.n1), 𝕀(n.n2))
+    Sz = kronecker(σz, 𝕀(n.n1), 𝕀(n.n2))
     for i in 1:length(ϵ)
         if (ϵ[i] - E_ground) < 1e-16
-            sz += tr(σz′*P[:,i]*adjoint(P[:,i]))
+            sz += tr(Sz*P[:,i]*adjoint(P[:,i]))
         end
     end
     return sz
 end
 
+#### T=0 sz magnetisation for TRANSFORMED Hamiltonian ####
 function szTzero′(prm::Lorentzian, ang::CouplingAngles, n::Levels)
     H = HTot′(prm, ang, n)
     F = eigen(H)
@@ -32,6 +34,7 @@ function szTzero′(prm::Lorentzian, ang::CouplingAngles, n::Levels)
     return sz
 end
 
+#### T=0 average number expectation for UNTRANSFORMED Hamiltonian ####
 function nTzero(prm::Lorentzian, ang::CouplingAngles, n::Levels) # Ground-state number expectation
     H = HTot(prm, ang, n)
     F = eigen(H)
@@ -50,6 +53,7 @@ function nTzero(prm::Lorentzian, ang::CouplingAngles, n::Levels) # Ground-state 
     return (n1 + n2)/2
 end
 
+#### T=0 average number expectation for TRANSFORMED Hamiltonian ####
 function nTzero′(prm::Lorentzian, ang::CouplingAngles, n::Levels) # Ground-state number expectation
     H = HTot′(prm, ang, n)
     F = eigen(H)
@@ -65,21 +69,20 @@ function nTzero′(prm::Lorentzian, ang::CouplingAngles, n::Levels) # Ground-sta
             n2 += tr(N2*P[:,i]*adjoint(P[:,i]))
         end
     end
-    return (n1 + n2)/2
+    return n1, n2
 end
 
+xmax(n) = 1/sqrt(2)*maximum(eigvals(X(n.n1)))
+
 function osc_positions(prm, ang, n)
-    probabilities = diag(ρGroundBath(prm, ang, n))
-    X1(n) = kronecker(𝕀(2), X(n.n1), 𝕀(n.n2))
-    X2(n) = kronecker(𝕀(2), 𝕀(n.n1), X(n.n2))
-    xmax = 1/sqrt(2)*maximum(eigvals(X(n.n1)))
-    xvals = LinRange(-1.5*xmax, 1.5*xmax, 500)
-    nvals = 1:length(probabilities)
-    dist = zeros(length(nvals), length(xvals))
+    probabilities = diag(ρGroundBath(prm, ang, n)) # Generates the probabilities for occupation of the oscillator levels
+    xvals = collect(LinRange(-1.5*xmax(n), 1.5*xmax(n), 500))  # Position sweep values
+    nvals = 1:length(probabilities) # All the possible oscillator levels
+    dist = zeros(length(xvals), length(nvals)) # Set up empty array for position distribution
     for i in 1:length(nvals)
-        basis = FockBasis(n.n1)
+        basis = FockBasis(n.n1) 
         psi = fockstate(basis, nvals[i])
-        dist[i,:] = probabilities[i]*sum(wigner(psi, xvals, xvals), dims=1)
+        dist[:,i] = probabilities[i]*sum(wigner(psi, xvals, xvals), dims=1)
     end
-    return xvals, sum(dist, dims=1)
+    return hcat(xvals, sum(dist, dims=2))
 end
