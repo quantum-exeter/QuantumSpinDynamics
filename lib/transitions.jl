6835856612dab1d36c188848b407adad07d046bf
Hogg <charlotte.hogg@me.com>
2023-06-28 15:12:03 +0100

    Comments added.

--- lib/transitions.jl
+++ lib/transitions.jl
@@ -2,7 +2,7 @@
 #### transitions.jl ####
 ########################
 
-### Jump Operators ###
+### Jump operators ###
 jump(n::Lev1D) = [kronecker(𝕀s, X(n.n1))]
 jump(n::Lev2D) = [kronecker(𝕀s, X(n.n1), 𝕀(n.n2)), kronecker(𝕀s, 𝕀(n.n1), X(n.n2))]
 jump(n::Lev3D) = [kronecker(𝕀s, X(n.n1), 𝕀(n.n2), 𝕀(n.n3)), kronecker(𝕀s, 𝕀(n.n1), X(n.n2), 𝕀(n.n3)), kronecker(𝕀s, 𝕀(n.n1), 𝕀(n.n2), X(n.n3))] 
@@ -18,7 +18,7 @@ function transitions(prm::Lorentzian, ang::CouplingAngles, n::Levels, i)
     eval = eigen(H).values
     evec = eigen(H).vectors
     proj(j) = evec[:,j]*adjoint(evec[:,j])
-    # Create a table of transition frequencies
+    # create a table of transition frequencies
     for k in 1:d
         for l in 1:d
             table[k, l] = eval[l] - eval[k]
