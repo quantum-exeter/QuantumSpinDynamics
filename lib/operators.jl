4e00bdf633dd154419460b9c96b40df4ed35d83f
Hogg <charlotte.hogg@me.com>
2023-06-28 15:05:45 +0100

    Comments added.

--- lib/operators.jl
+++ lib/operators.jl
@@ -2,10 +2,10 @@
 #### operators.jl ####
 ######################
 
-### Pauli Matrices ###
+### Pauli matrices ###
 σx = [[0 1];[1 0]]
 σy = [[0 -im];[im 0]]
 σz = [[1 0];[0 -1]]
 
-### Spin Coupling Operators ###
+### Spin coupling operators ###
 sc(θ, ϕ) = σx*(sin(θ)*cos(ϕ)) + σy*(sin(θ)*sin(ϕ)) + σz*cos(θ)
