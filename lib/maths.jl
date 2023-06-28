a18ab3c4c4b53c015c076fa48e954f2a3b4fe3ff
Hogg <charlotte.hogg@me.com>
2023-06-28 15:04:37 +0100

    Comments added and unused functions removed.

--- lib/maths.jl
+++ lib/maths.jl
@@ -2,48 +2,42 @@
 #### maths.jl ####
 ##################
 
-### Commutators/Anticommutators ###
-comm(A,B) = A*B - B*A
-acomm(A,B) = A*B + B*A
+### Commutators/anticommutators ###
+comm(A, B) = A*B - B*A # commutator of A and B
+acomm(A, B) = A*B + B*A # anticommutator of A and B
 
-### Square ###
-square(n) = n*n
+### Identity matrices ###
+𝕀(n) = Matrix(I, n, n) # identity matrix of dimension n
+𝕀s = 𝕀(2) # identity matrix of dimension 2
 
-### Identity Matrices ###
-𝕀(n) = Matrix(I, n, n) 
-𝕀s = 𝕀(2) # Spin
-
-### Partial Trace (Tracing Out Bath) ###
+### Partial trace (tracing out bath) ###
 function ptrace(ρ, n)
-    nR = Int(size(ρ, 1)/n) # This is the remaining dimension
+    nR = Int(size(ρ, 1)/n) # remaining dimension
     lhs(i) = kronecker(𝕀(nR), (𝕀(n)[[i],:]))
     rhs(i) = kronecker(𝕀(nR), (𝕀(n)[:,i]))
     return sum(lhs(i)*ρ*rhs(i) for i=1:n)
 end
 
-### Partial Trace (Tracing Out Spin) ###
+### Partial trace (tracing out spin) ###
 function ptraceSp(ρ)
   n = 2
-  nR = Int(size(ρ, 1)/n) # This is the remaining dimension
+  nR = Int(size(ρ, 1)/n) # remaining dimension
   lhs(i) = kronecker((𝕀(n)[[i],:]), 𝕀(nR))
   rhs(i) = kronecker((𝕀(n)[:,i]), 𝕀(nR))
   return sum(lhs(i)*ρ*rhs(i) for i=1:n)
 end
 
-### Uhlmann Fidelity ###
-fidelity(ρ1, ρ2) = square(tr(sqrt(sqrt(ρ1)*ρ2*sqrt(ρ1))))
-
-### Check for Choppable Components ###
-realIfClose(c) = isnan(imag(c)) || imag(c) < 1e-14 ? real(c) : c;
-realIfClose(c::AbstractArray) = realIfClose.(c);
-zeroIfClose(x) = abs(x) < 1e-12 ? zero(x) : x;
-chopReal(x) = real(x) < 1e-12 ? imag(x)*1im : x;
-chopImag(x) = imag(x) < 1e-12 ? real(x) : x;
-chopBoth(x) = chopReal(chopImag(x));
+### Check for choppable components ###
+realIfClose(c) = isnan(imag(c)) || imag(c) < 1e-14 ? real(c) : c; # returns the real part of c if the imaginary part is small or NaN
+realIfClose(c::AbstractArray) = realIfClose.(c); # returns the real part of each element of c if their imaginary part is small or NaN
+chopReal(x) = real(x) < 1e-12 ? imag(x)*1im : x; # chop the real components
+chopImag(x) = imag(x) < 1e-12 ? real(x) : x; # chop the imaginary components
+chopBoth(x) = chopReal(chopImag(x)); # chop both the real and imaginary components
 
 ### Integration ###
 xcoth(x) = iszero(x) ? one(x) : x*coth(x)
 
+## Double integration ##
 function dblquadgk(f, a::AbstractArray{T}, b::AbstractArray{T};
     rtol=sqrt(eps(T)), atol=zero(T), maxevals=10^7, order=7) where T<:AbstractFloat
 J(x) = quadgk(y -> f(x,y), a[2], b[2], atol=atol, maxevals=maxevals, order=order)[1]
@@ -51,6 +45,7 @@ K = quadgk(x -> J(x), a[1], b[1], atol=atol, maxevals=maxevals, order=order)[1]
 return K
 end
 
+## Principal value integrals with quadgk ##
 function quadgk_cauchy(f, a, c, b)
   fc = f(c)
   g(x) = (f(x) - fc)/(x - c)
