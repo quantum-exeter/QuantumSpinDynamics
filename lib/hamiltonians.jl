29c0bb6f1d9c2e0730f93b6a21bce5e0bf9159c9
Hogg <charlotte.hogg@me.com>
2023-06-28 15:43:14 +0100

    Comments added.

--- lib/hamiltonians.jl
+++ lib/hamiltonians.jl
@@ -30,21 +30,21 @@ N(n) = create(n)*annihilate(n)
 
 ### Hamiltonians ###
 
-## Spin hamiltonian ##
+## Spin Hamiltonian ##
 HS() = -sign(γ)*σz
 HS(n::Lev1D) = -sign(γ)*kronecker(σz, 𝕀(n.n1))
 HS(n::Lev2D) = -sign(γ)*kronecker(σz, 𝕀(n.n1), 𝕀(n.n2))
 HS(n::Lev3D) = -sign(γ)*kronecker(σz, 𝕀(n.n1), 𝕀(n.n2), 𝕀(n.n3))
 
-## Interaction hamiltonian ##
+## Interaction Hamiltonian ##
 HInt(prm::LorPrm1D, ang::CouplAng1D, n::Lev1D) = sqrt(3)*sqrt(prm.α1/prm.ω01)*kronecker(sc(ang.θ1, ang.ϕ1), X(n.n1))
 HInt(prm::LorPrm2D, ang::CouplAng2D, n::Lev2D) = sqrt(prm.α1/prm.ω01)*kronecker(sc(ang.θ1, ang.ϕ1), X(n.n1), 𝕀(n.n2)) + sqrt(prm.α2/prm.ω02)*kronecker(sc(ang.θ2, ang.ϕ2), 𝕀(n.n1), X(n.n2))
 HInt(prm::LorPrm3D, ang::CouplAng3D, n::Lev3D) = sqrt(prm.α1/prm.ω01)*kronecker(sc(ang.θ1, ang.ϕ1), X(n.n1), 𝕀(n.n2), 𝕀(n.n3)) + sqrt(prm.α2/prm.ω02)*kronecker(sc(ang.θ2, ang.ϕ2), 𝕀(n.n1), X(n.n2), 𝕀(n.n3)) + sqrt(prm.α3/prm.ω03)*kronecker(sc(ang.θ3, ang.ϕ3), 𝕀(n.n1), 𝕀(n.n2), X(n.n3))
 
-## Bath hamiltonian ##
+## Bath Hamiltonian ##
 HB(prm::LorPrm1D, n::Lev1D) = (1/s0)*(prm.ω01*kronecker(𝕀s, N(n.n1)))
 HB(prm::LorPrm2D, n::Lev2D) = (1/s0)*(prm.ω01*kronecker(𝕀s, N(n.n1), 𝕀(n.n2)) + prm.ω02*kronecker(𝕀s, 𝕀(n.n1), N(n.n2)))
 HB(prm::LorPrm3D, n::Lev3D) = (1/s0)*(prm.ω01*kronecker(𝕀s, N(n.n1), 𝕀(n.n2), 𝕀(n.n3)) + prm.ω02*kronecker(𝕀s, 𝕀(n.n1), N(n.n2), 𝕀(n.n3)) + prm.ω03*kronecker(𝕀s, 𝕀(n.n1), 𝕀(n.n2), N(n.n3)))
 
-## Total hamiltonian ##
+## Total Hamiltonian ##
 HTot(prm::Lorentzian, ang::CouplingAngles, n::Levels) = HS(n) + HInt(prm, ang, n) + HB(prm, n) 
