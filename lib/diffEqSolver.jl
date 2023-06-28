2639da03fc2cdd6f62f8da33a32e2d2bd350feec
Hogg <charlotte.hogg@me.com>
2023-06-28 14:32:42 +0100

    Comments added.

--- lib/diffEqSolver.jl
+++ lib/diffEqSolver.jl
@@ -5,13 +5,13 @@
 ### Algorithm ###
 alg = Vern7()
 
-### Differential Equation Solver ###
+### Differential equation solver ###
 function dsolve(prm::Lorentzian, ang::CouplingAngles, n::Levels, T, tspan)
     superop = 𝒮(prm, ang, n, T)
-    state_init = ρ0(prm, n, T)
-    dstate(dρ, ρ, v, t) = mul!(dρ, superop, ρ) # Solves the DE
+    state_init = ρ0(prm, n, T) # initial state
+    dstate(dρ, ρ, v, t) = mul!(dρ, superop, ρ) # solves the DE
     prob = ODEProblem(dstate, vec(state_init), tspan)
     solution = solve(prob, alg)
-    out(t) = reshape(solution(t), (hspace_size(n), hspace_size(n))) # Reformats vector into a density matrix
+    out(t) = reshape(solution(t), (hspace_size(n), hspace_size(n))) # reformats vector into a density matrix
     return out
 end
